add1eacf7088f7e2de49fea3abf8e578ff8e8ed8
EMMANUEL LUCKY <emmanueltech2019@gmail.com>
2024-06-22 16:37:54 +0100

    first commit

---
+++ src/app/data.tsx
@@ -0,0 +1,20 @@
+interface Data {
+    city: string;
+    state: string;
+    brand: string;
+    domain: string;
+    logo: string;
+    src: string;
+}
+const data:Data ={
+    brand:"OHS",
+    city:"Orlando",
+    state:"FL",
+    domain: "orlandoherniaspecialists.com",
+    logo: 'https://res.cloudinary.com/wise-solution-inc/image/upload/v1719070636/logo_2_pd8rkw.png',
+    src:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d224444.7544111507!2d-81.50747575357592!3d28.481073844149908!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e773d8fecdbc77%3A0xac3b2063ca5bf9e!2sOrlando%2C%20FL%2C%20USA!5e0!3m2!1sen!2sng!4v1719070555715!5m2!1sen!2sng"
+
+}
+
+
+export default data
